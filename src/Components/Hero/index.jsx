c1d0ffaf2a98ea5ec4458c581a71467e675c46a2
Dustin Apodaca <dustinapodaca@gmail.com>
2024-12-25 22:09:26 -0700

    updated SEO

--- src/Components/Hero/index.jsx
+++ src/Components/Hero/index.jsx
@@ -20,8 +20,8 @@ export default function Hero() {
       <div className="mx-auto max-w-7xl">
         <h1 className="sr-only">Ares Security</h1>
         <h2 className="sr-only">Colorado Security Services</h2>
-        <h2 className="sr-only">Denver, Colorado Springs, Pueblo</h2>
-        <h2 className="sr-only">Protection for People, Property and Plants</h2>
+        <h2 className="sr-only">Denver | Colorado Springs | Pueblo</h2>
+        <h2 className="sr-only">Protection for People, Property and Premises</h2>
         <div className="relative z-10 bg-black pb-8 sm:pb-16 md:pb-20 lg:w-full lg:max-w-2xl lg:pb-28 xl:pb-32">
           <svg
             className="absolute inset-y-0 right-0 hidden h-full w-48 translate-x-1/2 transform text-white lg:block"
